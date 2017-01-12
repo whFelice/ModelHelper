4c2f94feaf12e4b09c65fecfc458b25fcd3e0842
whFelice <hust_yin@163.com>
2017-01-12 11:21:42 +0800

    简单初始化

---
+++ Model.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * User: yinjiangchi
+ * Date: 2017/1/12
+ * Time: 9:23
+ */
+
+namespace whFelice\ModelHelper;
+
+
+class Model
+{
+
+}
---
+++ src/ModelHelperServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * User: yinjiangchi
+ * Date: 2017/1/12
+ * Time: 10:12
+ */
+class ModelHelperServiceProvider
+{
+    protected $defer = false;
+
+    public function boot()
+    {
+
+    }
+}
